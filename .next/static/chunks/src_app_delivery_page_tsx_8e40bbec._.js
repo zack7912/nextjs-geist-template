7e3e4c6544d9c701b206ff14467ed730af3c300d
zack7912 <rudraprasadd7912@gmail.com>
2025-07-16 20:07:37 +0530

    Add .next/static/chunks/src_app_delivery_page_tsx_8e40bbec._.js 1752676655718

---
+++ .next/static/chunks/src_app_delivery_page_tsx_8e40bbec._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_24211c3a._.js",
+  "static/chunks/src_d0e13ff4._.js"
+],
+    source: "dynamic"
+});
